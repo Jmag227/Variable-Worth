b8b364cff1087fb739b0b819cec6e41e85cff264
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-03 08:22:13 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int apexLifetimeMatches = 4650;
+int apexLiftimeKills = 4144;
+decimal kdrInApex = 0.88m;
+double season11Kdr = 1.13;
+string favLegend = "Mirage";
+char placeMent = '!';
+bool isJaromGood = true;
+
+Console.WriteLine("Hello! This is my program to brag about my favorite video game!");
+Console.WriteLine("it is a battle royal called Apex Legends");
+
+Console.WriteLine($"I have played so much, I have {apexLifetimeMatches} lifetime recorded matches, and {apexLiftimeKills} lifetime kills!");
+Console.WriteLine("So Apex has a very high skill set, and those are reflected in your kdr, or Kill to Death ratio,");
+Console.WriteLine($"My lifetime KDR is {kdrInApex}, but my kdr for the most recent season is {season11Kdr}!");
+Console.WriteLine($"There are many characters to pick from. My personal favorite is {favLegend}{placeMent}");
+Console.WriteLine($"so after seeing all those stats you're probably thinking that I am pretty good at the game...");
+Console.WriteLine("I'd personally say maybe... but lets see what my program says!");
+Console.WriteLine($"Fact check. is Jarom good at the game?...... fact check {isJaromGood}");
+
